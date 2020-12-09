f86350ddff7dc0e5305edd55e10186394f381c39
Daalin80 <73665109+Daalin80@users.noreply.github.com>
2020-12-09 13:55:41 +0200

    Delete ex2.py

--- ex2.py
+++
@@ -1,9 +0,0 @@
-def palindrom():
-    some_word = str(input("Please enter some word or palindrome: "))
-    word = some_word[::-1]
-    if some_word == word:
-        return "Yes, it`s palindrome"
-    else:
-        return "No, it`s not palindrome"
-
-print(palindrom())
